078a1a7318f1520628a43a9080e5e9a0e34bc733
Renan Santiago <renansantiago2013@hotmail.com>
2020-03-27 16:02:49 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';  //importando o useState para alterar o estado na interface
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Routes />   
+   );
 }
 
 export default App;
+
+
+// um componetne do react é uma função que retorna HTML
+// JSX = quando um HTML esta escrito dentro do javascript
+// toda vez que clicar no botão vai chamar o increment que aciona o counter += 1
+// um componente do react "ex: Header" tem sempre que começar com letra maisúscula
+ // o useState retorna um array [valor, atualização do valor]
